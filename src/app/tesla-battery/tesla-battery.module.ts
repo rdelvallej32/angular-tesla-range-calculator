1b06ae7e432d98e80a2d571db58a15558266269f
rdelvallej32 <rdelvallej@gmail.com>
2016-12-19 19:22:37 -0500

    Import tesla stats component into the module

--- src/app/tesla-battery/tesla-battery.module.ts
+++ src/app/tesla-battery/tesla-battery.module.ts
@@ -6,11 +6,13 @@ import { BatteryService } from './tesla-battery.service';
 
 import { TeslaBatteryComponent } from './containers/tesla-battery/tesla-battery.component';
 import { TeslaCarComponent } from './components/tesla-car/tesla-car.component';
+import { TeslaStatsComponent } from './components/tesla-stats/tesla-stats.component';
 
 @NgModule({
   declarations: [
     TeslaBatteryComponent,
-    TeslaCarComponent
+    TeslaCarComponent,
+    TeslaStatsComponent
   ],
   imports: [
     CommonModule,
