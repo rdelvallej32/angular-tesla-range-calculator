580f93ec7174e33767b0f7c2361cc928a5a521e9
rdelvallej32 <rdelvallej@gmail.com>
2017-01-09 19:41:34 -0500

    Add compression middleware to server file
    - Add compression middleware from npm middleware
    - Add comments to explain the purpose of each line for future self

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const config = require('./config/environment.js');
 const morgan = require('morgan');
+const compression = require('compression');
 
 // Get our API routes
 const api = require('./routes/api');
@@ -16,17 +17,26 @@ const app = express();
 const uri = process.env.MONGODB_URI || 'mongodb://localhost/ng2Tesla';
 mongoose.Promise = require('bluebird');
 mongoose.connect(uri);
+mongoose.connection.on('error', function(err) {
+  console.error(`MongoDB connection error  + ${err}`);
+  process.exit(-1); // eslint-disable-line no-process-exit
+});
+
+// // Point static path to dist
+app.set('appPath', path.join(config.root, 'dist'));
+app.use(express.static(app.get('appPath')));
+
+// Use morgan logger
+app.use(morgan('dev'));
+
+// Use compression middleware for improved performance
+app.use(compression());
 
 // Parsers for POST data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }));
-
-// // Point static path to dist
-app.set('appPath', path.join(config.root, 'dist'));
-app.use(express.static(app.get('appPath')));
-app.use(morgan('dev'));
 
 // Set our api routes
 app.use('/api', api);
